673b0f6d3ba4572bbd552302dde03edd9d3d78f1
sergio pons <serpons@gmail.com>
2021-01-06 19:59:06 +0100

    version2

--- JS Counter.js
+++ JS Counter.js
@@ -1,32 +1,56 @@
-var counter_list = [10,10000,10000];
-var str_counter_0 = counter_list[0];
-var str_counter_1 = counter_list[1];
-var str_counter_2 = counter_list[2];
-var display_str = "";
-var display_div = document.getElementById("display_div_id");
+(() => {
 
-function incrementCount(current_count){
-  setInterval(function(){
-    // clear count
-    while (display_div.hasChildNodes()) {
-        display_div.removeChild(display_div.lastChild);
+  let hours = `00`,
+      minutes = `00`,
+      seconds = `00`,
+      chronometerDisplay = document.querySelector(`[data-chronometer]`),
+      chronometerCall
+
+  function chronometer() {
+
+    seconds ++
+
+    if (seconds < 10) seconds = `0` + seconds
+
+    if (seconds > 59) {
+      seconds = `00`
+      minutes ++
+
+      if (minutes < 10) minutes = `0` + minutes
     }
-    str_counter_0++;
-    if (str_counter_0 > 99) {
-      str_counter_0 = 10; // reset count
-      str_counter_1++;    // increase next count
+
+    if (minutes > 59) {
+      minutes = `00`
+      hours ++
+      
+      if (hours < 10) hours = `0` + hours
     }
-    if(str_counter_1>99999){
-      str_counter_2++;
-    }
-    display_str = str_counter_2.toString() + str_counter_1.toString() + str_counter_0.toString();
-    for (var i = 0; i < display_str.length; i++) {
-      var new_span = document.createElement('span');
-      new_span.className = 'num_tiles';
-      new_span.innerText = display_str[i];
-      display_div.appendChild(new_span);
-    }
-  },1000)};
+
+    chronometerDisplay.textContent = `${hours}:${minutes}:${seconds}`
+
+  }
+
+  play.onclick = (event) => {
+    chronometerCall = setInterval(chronometer, 1000)
+    event.target.setAttribute(`disabled`,``)
+  }
+
+  pause.onclick = () => {
+    clearInterval(chronometerCall)
+    play.removeAttribute(`disabled`)
+  }
+
+  reset.onclick = () => {
+    clearInterval(chronometerCall)
+    play.removeAttribute(`disabled`)
+    chronometerDisplay.textContent = `00:00:00`
+    
+      hours = `00`,
+      minutes = `00`,
+      seconds = `00`
+  }
+  
+})()
   
 
 
